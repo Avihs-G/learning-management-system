1771c33a0f4ad8e5f5536ba7b42647ce3d39ba8f
Avihs-G <hirme4work@gmail.com>
2025-03-31 10:50:15 +0530

    Enter

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,16 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Use environment variables
 const firebaseConfig = {
-    apiKey: "AIzaSyAS2S5cSrX5PydB7MXigbMX8x7plVGxRZ0",
-    authDomain: "dlms-80ba4.firebaseapp.com",
-    projectId: "dlms-80ba4",
-    storageBucket: "dlms-80ba4.firebasestorage.app",
-    messagingSenderId: "695775562990",
-    appId: "1:695775562990:web:7465687be6a781196577ff",
-    measurementId: "G-9K6X24W70H"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
